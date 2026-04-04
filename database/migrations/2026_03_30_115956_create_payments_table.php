e21d829c256a8c63d6da6d7924f7611c25308065
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-04 11:16:28 -0700

    some changes

---
+++ database/migrations/2026_03_30_115956_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime("payment_date");
+            $table->string("payment_method");
+            $table->foreignId("employee_id")->constrained("employees")->onDelete("cascade");
+            $table->foreignId("payroll_id")->constrained("payrolls")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
