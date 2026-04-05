3f5b9064014a813eecde25d7fb373d34911b8b79
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-05 11:01:58 -0700

    some changes

---
+++ app/Models/Designation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use App\Models\Department;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Designation extends Model
+{
+    //
+    protected $fillable = [
+        "name",
+        "department_id"
+    ];
+    public function departments(){
+        return $this->belongsTo(Department::class);
+    }
+    public function employees(){
+        return $this->hasMany(Employee::class);
+    }
+}
