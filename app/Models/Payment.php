41d51d7f7e6b72c1360feb1f96229e8ab0ecd4f7
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-06 11:24:22 -0700

    some changes

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    //
+    protected $fillable = [
+        "payment_date",
+        "payment_method",
+        "employee_id",
+        "payrool_id"
+    ];
+    public function employee(){
+        return $this->belongsTo(Employee::class);
+    }
+    public function payroll(){
+        return $this->belongsTo(Payroll::class);
+    }
+}
