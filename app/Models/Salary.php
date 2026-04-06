10fc9d23feb54eba9332b4b8e6ecdf761879a96f
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-06 11:27:08 -0700

    some changes

---
+++ app/Models/Salary.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Salary extends Model
+{
+    //
+    protected $fillable = [
+        "gross_salary",
+        "employee_id",
+        "payroll_id"
+    ];
+    public function employee(){
+        return $this->belongsTo(Employee::class);
+    }
+    public function payroll(){
+        return $this->belongsTo(Payroll::class);
+    }
+}
