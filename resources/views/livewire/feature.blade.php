fba1804534906511ceb21a84504d1054b57a5793
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-10 10:15:46 -0700

    some changes

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -8,3 +8,14 @@
             <h1 class="text-3xl text-brand font-bold">About the School</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta impedit vero similique saepe aut quia sequi quaerat, exercitationem distinctio reprehenderit quas ratione debitis harum quasi excepturi iste doloribus voluptatum? Ipsum!</p>
             <div class="grid grid-cols-3 w-full gap-2">
+                    <div class="col-span-2">
+                    <h1 class="text-brand text-3xl font-bold">Manager Name</h1>
+                    <h1 class="text-4xl text-brand font-bold">Esmatullah Ghulami</h1>
+                    <p></p>
+                </div>
+                <img class="w-full h-44 border-4 border-brand rounded-md" src="images/home.jpg" alt="">
+            </div>
+        </div>
+        <div class="w-full h-72 rounded-xl shadow-[0px_0px_3px_rgba(0,0,0,0.4)] bg-white"></div>
+    </div>
+</div>
