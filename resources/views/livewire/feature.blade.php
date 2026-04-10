b22016e4b23176bd62632cbc6fa632a338a28b49
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-10 10:09:49 -0700

    some changes

---
+++ resources/views/livewire/feature.blade.php
@@ -0,0 +1,5 @@
+<div class="max-w-6xl mx-auto">
+    <div class="flex w-full flex-col gap-4 mt-6 items-center">
+        <h1 class="text-xl md:text:4xl lg:text-6xl font-bold">Sharifi High School</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, doloribus, aliquam, dolores labore nesciunt et esse autem assumenda id accusantium sed eaque maxime quod deleniti provident ab quaerat soluta obcaecati?</p>
+    </div>
