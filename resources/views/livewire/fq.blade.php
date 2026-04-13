add4e2e40b57bacea98bf162b54a3db1eefabbc4
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-12 19:19:50 -0700

    fq section done

--- resources/views/livewire/fq.blade.php
+++ resources/views/livewire/fq.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="w-full grid grid-cols-2 gap-3 max-w-6xl my-4 mx-auto">
+    <div class="w-full col-span-1">
+        <h1 class="">Frequently Asked Questions</h1>
+    </div>
+    <div class="col-span-2 w-full flex flex-col items-start">
+        @foreach ($this->faqs as $idx=>$faq)
+            <div wire:key="{{ $idx }}" class="w-full flex justify-between">
+                <p>{{ $faq["question"] }}</p>
+                <i>∨</i>
+            </div>
+        @endforeach
+    </div>
 </div>
