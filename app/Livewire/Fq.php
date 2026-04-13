6c47f58ecdcceb0c3eb2c6c1e3c45c2d1b4832f6
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-12 19:21:15 -0700

    fq section done

--- app/Livewire/Fq.php
+++ app/Livewire/Fq.php
@@ -11,3 +11,11 @@ class Fq extends Component
         "question" => "What is Austad Sharifi High School?",
         "answer" => "Austad Sharifi High School is a governmental (public) school that provides education based on Afghanistan's national curriculum.",
         ],
+         [
+            "question" => "Who was the school named after?",
+            "answer" => "The school is named in honor of Ramazan Ali Sharifi for his contributions to education and the Hazara community.",
+        ],
+        [
+            "question" => "Is the School public or private?",
+            "answer" => "It is a public (government-run) school."
+        ],
