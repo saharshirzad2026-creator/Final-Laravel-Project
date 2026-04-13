e771a45e294b3f974d6ab4eb2327041c0a42c847
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-12 19:20:37 -0700

    fq section done

---
+++ app/Livewire/Fq.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Fq extends Component
+{
+    public $faqs = [
+        [
+        "question" => "What is Austad Sharifi High School?",
+        "answer" => "Austad Sharifi High School is a governmental (public) school that provides education based on Afghanistan's national curriculum.",
+        ],
