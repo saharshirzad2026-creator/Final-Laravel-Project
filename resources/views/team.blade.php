9b723e4f5aa2a947cd14502ad242b2f23828573b
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-17 22:27:18 -0700

    team section

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,5 +1,9 @@
 <div class="w-full flex">
-    <div>
+    <div class="">
        <img src="images/home.jpg" alt="">
+       <h1>Name</h1>
+       <h1>LastName</h1>
+       <h1>Role</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit tenetur iste ducimus repudiandae animi magnam aliquid ipsum at, a fugiat temporibus expedita vero delectus quis provident veniam suscipit odio!</p>
     </div>
 </div>
