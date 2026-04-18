f33b9298b9a7b27d707f1a098b997cc61e558a59
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-17 22:31:32 -0700

    team section

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,9 +1,16 @@
-<div class="w-full flex">
+<div class="w-full flex justify-between gap-2">
     <div class="">
        <img class="w-full h-52 rounded-full" src="images/home.jpg" alt="">
        <h1 class="text-2xl text-center">Name</h1>
        <h1 class="text-2xl text-center">LastName</h1>
        <h1 class="text-2xl text-center">Role</h1>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit tenetur iste ducimus repudiandae animi magnam aliquid ipsum at, a fugiat temporibus expedita vero delectus quis provident veniam suscipit odio!</p>
+       <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit tenetur iste ducimus repudiandae animi magnam aliquid ipsum at, a fugiat temporibus expedita vero delectus quis provident veniam suscipit odio!</p>
+    </div>
+    <div class="">
+       <img class="w-full h-52 rounded-full" src="images/home.jpg" alt="">
+       <h1 class="text-2xl text-center">Name</h1>
+       <h1 class="text-2xl text-center">LastName</h1>
+       <h1 class="text-2xl text-center">Role</h1>
+       <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit tenetur iste ducimus repudiandae animi magnam aliquid ipsum at, a fugiat temporibus expedita vero delectus quis provident veniam suscipit odio!</p>
     </div>
 </div>
