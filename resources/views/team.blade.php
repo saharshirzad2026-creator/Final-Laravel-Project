24c3aac9644455d702d3da553e5bd9e2ec4b10df
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-17 22:29:38 -0700

    team section

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,9 +1,9 @@
 <div class="w-full flex">
     <div class="">
-       <img src="images/home.jpg" alt="">
-       <h1>Name</h1>
-       <h1>LastName</h1>
-       <h1>Role</h1>
+       <img class="w-full h-52 rounded-full" src="images/home.jpg" alt="">
+       <h1 class="text-2xl text-center">Name</h1>
+       <h1 class="text-2xl text-center">LastName</h1>
+       <h1 class="text-2xl text-center">Role</h1>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit tenetur iste ducimus repudiandae animi magnam aliquid ipsum at, a fugiat temporibus expedita vero delectus quis provident veniam suscipit odio!</p>
     </div>
 </div>
