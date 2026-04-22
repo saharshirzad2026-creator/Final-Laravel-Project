8f3b1792e2f0bb813ee3f2883b6a6fdef27afbf9
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-21 18:50:09 -0700

    seeders made

---
+++ database/seeders/SalarySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SalarySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('salaries')->insert([
+            "gross_salary"=> ""
+        ]);
+    }
+}
