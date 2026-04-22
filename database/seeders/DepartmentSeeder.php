6e39f56cf064115ccb6bdab90095dae2e808eef9
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-21 18:48:03 -0700

    seeders made

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('departments')->insert([
+            "name"=> "Computer",
+            "school_id" => 1
+        ]);
+    }
+}
