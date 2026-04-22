afa6c3fddc3d42f4f97d4be1761259f9519a62e6
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-21 18:49:36 -0700

    seeders made

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('employees')->insert([
+            "firstName"=> "Ali",
+            "lastName"=> "Rezaie",
+            "tazkira"=> "34567896423451",
+            "phone"=> "0765432178",
+            "email"=> "alirezaie@gmail.com",
+            "designation_id"=> 2
+        ]);
+    }
+}
