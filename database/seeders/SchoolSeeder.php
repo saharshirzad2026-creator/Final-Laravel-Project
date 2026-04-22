813767ef410c50058fe4582b73a767735032187d
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-21 18:50:58 -0700

    seeders made

---
+++ database/seeders/SchoolSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SchoolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('schools')->insert([
+            [
+               "name"=> "Sharifi High School",
+               "whatsApp_link"=> "0987645332",
+            ],
+            [
+               "name"=> "Bibi Zahra High School",
+               "whatsApp_link"=> "0786543219",
+            ]
+        ]);
+    }
+}
