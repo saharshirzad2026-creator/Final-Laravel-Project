27aae7b8060b50dac13de6fedc8b93fe6c820ef5
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-23 22:33:11 -0700

    edit and index of contract

--- app/Livewire/Contract/Edit.php
+++ app/Livewire/Contract/Edit.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire\Contract;
 
+use App\Models\Contract;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    public $id;
+    public $contract;
+    public function update(Request $request, String $id){
+        $this->id = $id;
+    }
+    public function mount(){
+        $this->contract = Contract::findOrFail($this->id);
+    }
     public function render()
     {
         return view('livewire.contract.edit');
--- app/Livewire/Contract/Index.php
+++ app/Livewire/Contract/Index.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire\Contract;
 
+use App\Models\Contract;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $contract;
+    public function mount(){
+        $this->contract = Contract::all();
+    }
     public function render()
     {
         return view('livewire.contract.index');
