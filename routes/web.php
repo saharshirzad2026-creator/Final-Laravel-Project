429f4c1d557cc92ed991bd1223f7e277b94fe658
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-25 07:25:34 -0700

    routes of designation employee and contract

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,27 @@ Route::middleware(['auth','verified'])->group(function(){
     });
 
     Route::prefix("department")->name("deaprtment.")->group(function(){
-        Route::get("/", Index::class)->name("index");
-        Route::get("/add", Add::class)->name("add");
-        Route::get("{id}/edit", Edit::class)->name("edit");
+        Route::get("/", DepartmentIndex::class)->name("index");
+        Route::get("/add", DepartmentAdd::class)->name("add");
+        Route::get("{id}/edit", DepartmentEdit::class)->name("edit");
+    });
+
+    Route::prefix("designations")->name("designations.")->group(function(){
+        Route::get("/", DesignationsIndex::class)->name("index");
+        Route::get("/add", DesignationsAdd::class)->name("add");
+        Route::get("{id}/edit", DesignationsEdit::class)->name("edit");
+    });
+
+    Route::prefix("employees")->name("employees.")->group(function(){
+        Route::get("/", EmployeeIndex::class)->name("index");
+        Route::get("/add", EmployeeAdd::class)->name("add");
+        Route::get("{id}/edit", EmployeeEdit::class)->name("edit");
+    });
+
+    Route::prefix("contract")->name("contract.")->group(function(){
+        Route::get("/", ContractIndex::class)->name("index");
+        Route::get("/add", ContractAdd::class)->name("add");
+        Route::get("{id}/edit", ContractEdit::class)->name("edit");
     });
 });
 
